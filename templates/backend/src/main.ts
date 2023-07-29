e80851776db708aa4bb73bc8c2b4822d4db73b28
GandoPlay <38758488+GandoPlay@users.noreply.github.com>
2023-07-29 22:35:52 +0300

    Update main.ts

--- templates/backend/src/main.ts
+++ templates/backend/src/main.ts
@@ -8,13 +8,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  app.use(
-    session({
-      secret: 'my-secret',
-      resave: false,
-      saveUninitialized: false, 
-    }),
-  );
+
 
   app.useGlobalPipes(new ValidationPipe());
 
